e32e5f0a139cfff9d2a876750ea9fd4c026c17d8
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-15 09:36:49 -0500

    first commit

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+let product = [  // [name, category, price, inventory]
+    ["10x pack pens", "Office supplies", 4.99, 102],
+    ["Oreos","Snack", 5.99, 54],
+    ["6x pack T shirts", "Clothing", 4.99, 86],
+    ["Computer Mouse", "Computer supplies", 14.99, 29],
+    ["Lucky Strike Cigarettes", "Cigarettes", 9.99, 17]
+]
