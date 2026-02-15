307bf017dfe2ed943c7272cc1ca595144f7b411b
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-15 10:31:14 -0500

    successful first use of "for ... of"" for price changing

--- cc_4.js
+++ cc_4.js
@@ -1,8 +1,8 @@
 let product = [  // [name, category, price, inventory]
     ["10x pack pens", "Office supplies", 4.99, 102],
-    ["Oreos","Snack", 5.99, 54],
-    ["6x pack T shirts", "Clothing", 4.99, 86],
-    ["Computer Mouse", "Computer supplies", 14.99, 29],
+    ["Oreos","groceries", 5.99, 54],
+    ["6x pack T shirts", "apparel", 4.99, 86],
+    ["Computer Mouse", "electronics", 14.99, 29],
     ["Lucky Strike Cigarettes", "Cigarettes", 9.99, 17]
 ]
 
@@ -14,4 +14,31 @@ product.forEach(element => {
     const inventory = element[3]
 
     console.log(`${name} | ${category} | $${price} | Stock: ${inventory}`)
+
 });
+
+console.log("        ")
+
+for (element of product)
+    if (element[1] == "electronics"){
+        element[2] *= .8
+        break
+    }
+
+product.forEach(element => {
+    const name = element[0]
+    const category = element[1]
+    const price = element[2]
+    const inventory = element[3]
+
+    console.log(`${name} | ${category} | $${price} | Stock: ${inventory}`)
+
+});
+
+
+
+
+//    "electronics": 20% off
+//    "apparel": 15% off
+//    "groceries" and "household": 10% off
+//    default: No discount
